93c0a0a2ff27aa34e2c8d11c28b5e24d8ffb23f0
Dan Horan <dan.horan@me.com>
2025-07-18 20:36:15 +0100

    Added tailwindcss

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Dan Horan';
+export const SITE_TITLE = 'Dan Horan – Developer & Engineering Leader';
 export const SITE_DESCRIPTION = 'Experienced engineering leader and software engineer blending deep technical skills with a people-first mindset. Looking for my next opportunity to build and lead.';
+export const HEADER_TITLE = 'Dan Horan';
