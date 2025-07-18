e5fc364e96108ebea7528d1381f946dd07a20dbb
Dan Horan <dan.horan@me.com>
2025-07-18 19:15:52 +0100

    Updated styles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dan Horan';
+export const SITE_DESCRIPTION = 'Experienced engineering leader and software engineer blending deep technical skills with a people-first mindset. Looking for my next opportunity to build and lead.';
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,8 +2,8 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Load Markdown and MDX files in the `src/content/thoughts/` directory.
+	loader: glob({ base: './src/content/thoughts', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) => z.object({
 		title: z.string(),
